ae3a09ca729effd7faea3f7008cad38c488adf7d
arzummammadova <arzuam-af106@code.edu.az>
2025-10-19 18:00:56 +0400

    page

--- src/app/(home)/_components/Hero.tsx
+++ src/app/(home)/_components/Hero.tsx
@@ -8,20 +8,24 @@ const Hero = () => {
     <div className="text-center">
       <Title />
 
-      <div className="relative w-full 
-    h-[800px]        /* large desktops */
-    lg:h-[700px] 
-    md:h-[600px]     
-    sm:h-[300px]     /* tablets & small phones */
-    h-[300px]        /* extra small (default) */
-">
-  <Image
-    src="https://static.wixstatic.com/media/9c608a_30e7a1feae5c4d8a96f3cd1261fd4658.jpg"
-    alt="Construction hero image"
-    fill
-    className="object-cover"
-  />
-</div>
+      <div
+        className="
+    relative w-full
+    h-[300px]          /* default (extra small) */
+    sm:h-[400px]       /* small screens ≥640px */
+    md:h-[600px]       /* medium ≥768px */
+    lg:h-[700px]       /* large ≥1024px */
+    xl:h-[800px]       /* extra large ≥1280px */
+  "
+      >
+        <Image
+          src='https://static.wixstatic.com/media/9c608a_30e7a1feae5c4d8a96f3cd1261fd4658.jpg'
+          alt='Construction hero image'
+          fill
+          className='object-cover'
+        />
+      </div>
+
 
 
       <div className="w-[60%] mx-auto py-[100px] flex flex-col gap-8 items-center">
