a2b6fe572496f10319952f03db049a510dc8f798
arzummammadova <arzuam-af106@code.edu.az>
2025-10-24 23:52:08 +0400

    l:

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 const Title = () => {
-    return (
-        <div className="py-[100px] text-center">
-            <h1 className='text-[54px] font-bold'>Nehräm Entreprenad AB</h1>
-            <p className='text-[28px] italic font-bold text-[#8b0000]'>
-                Bygg och renovering
-            </p>
-        </div>
-    )
+  return (
+    <div className="py-[60px] text-center">
+      <h1 className="text-[28px] sm:text-[40px] md:text-[54px] font-bold">
+        Nehräm Entreprenad AB
+      </h1>
+      <p className="text-[16px] sm:text-[20px] md:text-[28px] italic font-bold text-[#8b0000]">
+        Bygg och renovering
+      </p>
+    </div>
+  )
 }
 
 export default Title
